a5b87b20f154b9c0b81f54e4e9550a662afbc082
Hugo <boyer.hugo974@gmail.com>
2026-08-07 23:15:02 +0200

    ajout du component et des fichier Meteo.jsx et Meteo.css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Meteo from './components/Meteo'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Meteo />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Meteo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Meteo.css"
+const Meteo = () => {
+  return (
+    <h1>Meteo</h1>
+  )
+}
+
+export default Meteo
