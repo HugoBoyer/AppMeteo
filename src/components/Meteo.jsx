15f200f0cefdc2a8d577b611f730631d03484087
Hugo <boyer.hugo974@gmail.com>
2026-08-08 00:25:03 +0200

    jout de la class meteo-data et col sur les div dans Meteo.jsx et ajout de fonction async appelant l'api openweathermap

--- src/components/Meteo.jsx
+++ src/components/Meteo.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Meteo.css"
-import search from "../assets/chercher.png"
+import search_icon from "../assets/chercher.png"
 import brume_icon from "../assets/brume.png"
 import drizzle_icon from "../assets/drizzle.png"
 import neigeux_icon from "../assets/neigeux.png"
@@ -12,15 +12,49 @@ import vent_icon from "../assets/vent.png"
 
 
 const Meteo = () => {
+
+ const search = async (city) => {
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+    
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+    } catch (error) {
+
+    }
+ }
+
+ useEffect(() => {
+    search("London")
+ }, [])
+
   return (
     <div className='meteo'>
         <div className="search-bar">
             <input type="text" placeholder='Search'/>
-            <img src={search} alt=""/>
+            <img src={search_icon} alt=""/>
         </div>
-        <img src={brume_icon} alt="" className='meteo-icon'/>
+        <img src={soleil_icon} alt="" className='meteo-icon'/>
         <p className='temperature'>16°C</p>
         <p className='location'>Londre</p>
+        <div className="meteo-data">
+            <div className="col">
+                <img src={brume_icon} alt="" />
+                <div>
+                    <p>91 %</p>
+                    <span>Humiditer</span>
+                </div>
+            </div>
+            <div className="col">
+                <img src={vent_icon} alt="" />
+                <div>
+                    <p>3.6 Km/h</p>
+                    <span>Vitesse du vent</span>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
