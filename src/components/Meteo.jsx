1406be2d3dd723e4b13c2afae1f3eb16deb16aae
Hugo <boyer.hugo974@gmail.com>
2026-08-07 23:46:02 +0200

    ajout des balise p dans Meteo.jsx et ajout de l'icon meteo dans Meteo.jsx

--- src/components/Meteo.jsx
+++ src/components/Meteo.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import "./Meteo.css"
 import search from "../assets/chercher.png"
+import brume_icon from "../assets/brume.png"
+import drizzle_icon from "../assets/drizzle.png"
+import neigeux_icon from "../assets/neigeux.png"
+import nuageux_icon from "../assets/nuageux.png"
+import pluvieux_icon from "../assets/pluvieux.png"
+import soleil_icon from "../assets/soleil.png"
+import temps_icon from "../assets/temps.png"
+import vent_icon from "../assets/vent.png"
+
 
 const Meteo = () => {
   return (
@@ -9,6 +18,9 @@ const Meteo = () => {
             <input type="text" placeholder='Search'/>
             <img src={search} alt=""/>
         </div>
+        <img src={brume_icon} alt="" className='meteo-icon'/>
+        <p className='temperature'>16°C</p>
+        <p className='location'>Londre</p>
     </div>
   )
 }
