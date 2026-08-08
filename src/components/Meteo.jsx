b47f8151f72ffebe3c6466cba409ede0b3b66898
Hugo <boyer.hugo974@gmail.com>
2026-08-08 15:37:40 +0200

    ajout des valeur api de l'humiditer, la vitesse du vent , la temperature , la location , et de l'icon sur le front de la page creation d'un objet pour utilliser ces propre image et de l'utilisation de MeteoData qui rassemble toute les valeur de l'api recuperer

--- src/components/Meteo.jsx
+++ src/components/Meteo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Meteo.css"
 import search_icon from "../assets/chercher.png"
 import brume_icon from "../assets/brume.png"
@@ -7,19 +7,46 @@ import neigeux_icon from "../assets/neigeux.png"
 import nuageux_icon from "../assets/nuageux.png"
 import pluvieux_icon from "../assets/pluvieux.png"
 import soleil_icon from "../assets/soleil.png"
-import temps_icon from "../assets/temps.png"
 import vent_icon from "../assets/vent.png"
-
+import tempete_icon from "../assets/tempete.png"
+import couvert_icon from "../assets/couvert.png"
 
 const Meteo = () => {
 
+ const [meteoData, setMeteoData] = useState(false)
+ const allIcons = {
+    "01d": soleil_icon,
+    "01n": soleil_icon,
+    "02d": nuageux_icon,
+    "02n": nuageux_icon,
+    "03d": couvert_icon,
+    "03n": couvert_icon,
+    "04d": couvert_icon,
+    "04n": couvert_icon,
+    "09d": drizzle_icon,
+    "09n": drizzle_icon,
+    "10d": pluvieux_icon,
+    "10n": pluvieux_icon,
+    "11d": tempete_icon,
+    "11n": tempete_icon,
+    "13d": neigeux_icon,
+    "13n": neigeux_icon,
+ }
  const search = async (city) => {
     try {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
     
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
+        const icon = allIcons[data.weather[0].icon] || soleil_icon;
+        setMeteoData({
+            himidity: data.main.himidity,
+            windSpeed: data.wind.speed,
+            temperature: Math.floor(data.main.temp),
+            location: data.name,
+            icon: icon
+        })
 
     } catch (error) {
 
@@ -37,20 +64,20 @@ const Meteo = () => {
             <img src={search_icon} alt=""/>
         </div>
         <img src={soleil_icon} alt="" className='meteo-icon'/>
-        <p className='temperature'>16°C</p>
-        <p className='location'>Londre</p>
+        <p className='temperature'>{meteoData.temperature}°C</p>
+        <p className='location'>{meteoData.location}</p>
         <div className="meteo-data">
             <div className="col">
                 <img src={brume_icon} alt="" />
                 <div>
-                    <p>91 %</p>
+                    <p>{meteoData.humidity} %</p>
                     <span>Humiditer</span>
                 </div>
             </div>
             <div className="col">
                 <img src={vent_icon} alt="" />
                 <div>
-                    <p>3.6 Km/h</p>
+                    <p>{meteoData.windSpeed} Km/h</p>
                     <span>Vitesse du vent</span>
                 </div>
             </div>
